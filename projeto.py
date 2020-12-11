1fe9e1498587565124f88aba4b3c3062c76818de
GlauberFerreiraAngelo <42098584+GlauberFerreiraAngelo@users.noreply.github.com>
2020-12-10 21:49:54 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,4 +1,5 @@
-arquivo = open('instance4.data','r')
+import numpy as np
+arquivo = open('instance2.data','r')
 
 numero_de_postos = 0
 numero_de_habitantes = 0
@@ -22,13 +23,12 @@ for n_lin in range(numero_de_postos):
     linha.append(int(teste[0]))
     linha.append(int(teste[1]))
     matriz.append(linha)
-print(matriz)
+#print(matriz)
 
 arquivo.readline()
 arquivo.readline()
 
 matrizHxP = []
-limpa_espaco = 0;
 for n_lin in range(numero_de_habitantes):
     linha2 = [] # cria uma linha para a matriz
     teste2 =  arquivo.readline().split(" ")
@@ -37,12 +37,62 @@ for n_lin in range(numero_de_habitantes):
             linha2.append(int(teste2[n_col]))
             
     matrizHxP.append(linha2)
-print(matrizHxP)
+#print(matrizHxP)
 
 
+for n_lin in range(numero_de_postos):
+    print(matriz[n_lin][0], matriz[n_lin][1])
+
+
+aux=""
+for n_lin in range(numero_de_habitantes):
+    for n_col in range(numero_de_postos):
+        aux = aux+""+str(matrizHxP[n_lin][n_col])
 
 print('numero de postos: ', numero_de_postos )
 print('numero de habitantes: ', numero_de_habitantes )
 print('numero de postos escolhidos: ', numero_de_postos_escolhidos )
 
 arquivo.close()
+
+vetorVerificar = []
+quantidade = numero_de_postos_escolhidos
+matriz_numero_de_postos_escolhidos = []
+while(quantidade > 0):
+    maior = 0
+    ID_do_posto = 0
+
+    linha2 = [] # cria uma linha para a matriz
+    
+    Raio_de_cobertura_do_posto = 0
+    for n_lin in range(numero_de_postos):
+       if(matriz[n_lin][1] >= maior):
+           if(matriz[n_lin][0] in vetorVerificar):
+               b = 0
+           else:    
+               maior = matriz[n_lin][1]
+               ID_do_posto = matriz[n_lin][0]
+               Raio_de_cobertura_do_posto = matriz[n_lin][1]         
+
+    linha2.append(ID_do_posto)
+    linha2.append(Raio_de_cobertura_do_posto)
+    matriz_numero_de_postos_escolhidos.append(linha2)
+    vetorVerificar.append(ID_do_posto)
+    quantidade -= 1
+
+print(matriz_numero_de_postos_escolhidos)
+
+def imprime_matriz(matriz):
+
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+
+    for i in range(linhas):
+        for j in range(colunas):
+            if(j == colunas - 1):
+                print("%d" %matriz[i][j])
+            else:
+                print("%d" %matriz[i][j], end = " ")
+    print()
+
+imprime_matriz(matrizHxP)  
